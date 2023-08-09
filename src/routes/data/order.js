c2ccc1856caec8d956be236ea83d434aa8546e4c
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-08-10 01:41:39 +0200

    Styling and cmongoose ofig

---
+++ src/routes/data/order.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+    name: String,
+    address: String,
+    email: String,
+    item: [{name: String, quantity: Number}],
+    createdAt: {type: Date, default: Date.now }
+})
+
+const order = mongoose.model('order', orderSchema)
+
+module.exports = {order}
